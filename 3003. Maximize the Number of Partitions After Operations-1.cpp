af80c4bed7da35de457e2384dc3c45fb52ce6ceb
Your Name <your.email@example.com>
2025-10-17 15:10:50 +0800

    2025/10/17

---
+++ 3003. Maximize the Number of Partitions After Operations-1.cpp
@@ -0,0 +1,55 @@
+struct Node {
+  int seg, mask, cnt;
+};
+class Solution {
+public:
+  int maxPartitionsAfterOperations(string s, int k) {
+    if (k == 26)
+      return 1;
+    int n = s.size();
+
+    vector<Node> pref(n), suff(n);
+
+    int seg = 0, mask = 0, cnt = 0;
+    for (int i = 0; i < n - 1; ++i) {
+      int x = s[i] - 'a';
+      if ((mask >> x & 1) == 0) {
+        if (++cnt > k) {
+          seg++;
+          mask = 0;
+          cnt = 1;
+        }
+        mask |= 1 << x;
+      }
+      pref[i + 1] = {seg, mask, cnt};
+    }
+
+    seg = mask = cnt = 0;
+    for (int i = n - 1; i > 0; --i) {
+      int x = s[i] - 'a';
+      if ((mask >> x & 1) == 0) {
+        if (++cnt > k) {
+          seg++;
+          mask = 0;
+          cnt = 1;
+        }
+        mask |= 1 << x;
+      }
+      suff[i - 1] = {seg, mask, cnt};
+    }
+
+    // pref[i] = s[0 ... i - 1]
+    // suff[i] = s[i + 1 ... n - 1]
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+      auto [segL, maskL, cntL] = pref[i];
+      auto [segR, maskR, cntR] = suff[i];
+      mask = maskL | maskR;
+      int bc = __builtin_popcount(mask);
+      int add = (bc + 1) <= k ? 1 : (cntL == k && cntR == k && bc < 26) ? 3 : 2;
+      res = max(res, segL + segR + add);
+    }
+
+    return res;
+  }
+};
