ef08bc3adc5cba883831d69d2a4ba0b12f885098
Your Name <your.email@example.com>
2025-10-20 12:23:43 +0800

    2025/10/20

---
+++ 2011. Final Value of Variable After Performing Operations-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  int finalValueAfterOperations(vector<string> &operations) {
+    int X = 0;
+    for (auto &op : operations)
+      if (op == "X++" || op == "++X")
+        X += 1;
+      else
+        X -= 1;
+
+    return X;
+  }
+};
